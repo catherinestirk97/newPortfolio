bba85959c0b2a86cd45f52358901b70ac296bda7
catherinestirk <cathystirky@gmail.com>
2021-02-06 12:58:50 +0000

    add scroll js

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function typeWriter() {
 }
 
 typeWriter();
+
+function scrollToAbout() {
+    document.querySelector('.about').scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+}
+
+function scrollToProjects() {
+    document.querySelector('.projects').scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+}
+
+function scrollToContact() {
+    document.querySelector('.contact').scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+}
